faf02625bab866c1b035108e51b5739272bd3dc5
JOSÉ LUIS MUÑOZ PADILLA <USIS043721@ugb.edu.sv>
2021-09-26 21:51:08 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Data.OleDb;
+
+namespace Venta_de_dispositivos_tecnologicos
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conexion_access = new OleDbConnection();
+            conexion_access.ConnectionString = @"Provider = Microsoft.Jet.OLEDB.4.0; Data Source = C:\sistema\sistema.mdb;Persist Security Info=False;";
+
+            conexion_access.Open();
+
+            OleDbDataAdapter consulta = new OleDbDataAdapter("SELECT * FROM tusuario", conexion_access);
+
+            DataSet resultado = new DataSet();
+            consulta.Fill(resultado);
+
+            foreach (DataRow registro in resultado.Tables[0].Rows)
+            {
+                if ((textBox1.Text == registro["nombre"].ToString()) && (textBox2.Text == registro["clave"].ToString()))
+                {
+                    fmenu fm = new fmenu();
+                    fm.Show();
+                    this.Hide();
+                }
+            }
+            try
+            {
+
+            }
+        
+
+        catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+                MessageBox.Show("Error de usuario o clave de acceso", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Focus();
+            }
+            conexion_access.Close();
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conexion = new SqlConnection(@"server=.\DESKTOP-PMRS7B8\SQL4; Initial Catalog = master; Integrated Security=True;");
+                conexion.Open();
+
+                string consultax;
+                consultax = "select usuario, pass from Usuario where usuario = '" + textBox1.Text + "'And pass = '" + textBox2.Text + "' ";
+                SqlCommand consulta = new SqlCommand(consultax, conexion);
+                consulta.ExecuteNonQuery();
+                DataSet ds = new DataSet();
+                SqlDataAdapter da = new SqlDataAdapter(consulta);
+
+                da.Fill(ds, "usuario");
+
+                DataRow registro;
+                registro = ds.Tables["usuario"].Rows[0];
+
+                if ((textBox1.Text == registro["usuario"].ToString()) || (textBox2.Text == registro["pass"].ToString()))
+                {
+                    fmenu fm = new fmenu();
+                    fm.Show();
+                    this.Hide();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error de usuario o clave de acceso", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    
+
+    private void button3_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conectar = new MySqlConnection("server=localhost;database=sistema_usuarios;id=root;password=grupocuatro;");
+            conectar.Open();
+
+            MySqlCommand codigo = new MySqlCommand();
+            MySqlConnection conectanos = new MySqlConnection();
+            codigo.Connection = conectar;
+
+            codigo.CommandText = ("Select *from usuarios where usuarios= '" + textBox1.Text + "' and password = '" + textBox2 + "'  ");
+
+            MySqlDataReader leer = codigo.ExecuteReader();
+
+            if (leer.Read())
+            {
+                MessageBox.Show("Bienvenidos");
+                fmenu fm = new fmenu();
+                fm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o Contraseña incorrectos");
+            }
+            conectar.Close();
+
+        }
+    }
+}
---
+++ fmenu.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace Venta_de_dispositivos_tecnologicos
+{
+    partial class fmenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.productosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pCsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.laptosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.celularesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.descuentosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.empleadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sALIRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Blue;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.productosToolStripMenuItem,
+            this.descuentosToolStripMenuItem,
+            this.empleadosToolStripMenuItem,
+            this.ayudaToolStripMenuItem,
+            this.acercaDeToolStripMenuItem,
+            this.sALIRToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(790, 29);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // productosToolStripMenuItem
+            // 
+            this.productosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pCsToolStripMenuItem,
+            this.laptosToolStripMenuItem,
+            this.celularesToolStripMenuItem});
+            this.productosToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.productosToolStripMenuItem.Name = "productosToolStripMenuItem";
+            this.productosToolStripMenuItem.Size = new System.Drawing.Size(99, 25);
+            this.productosToolStripMenuItem.Text = "Productos";
+            // 
+            // pCsToolStripMenuItem
+            // 
+            this.pCsToolStripMenuItem.Name = "pCsToolStripMenuItem";
+            this.pCsToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+            this.pCsToolStripMenuItem.Text = "PC`s";
+            // 
+            // laptosToolStripMenuItem
+            // 
+            this.laptosToolStripMenuItem.Name = "laptosToolStripMenuItem";
+            this.laptosToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+            this.laptosToolStripMenuItem.Text = "Laptos";
+            // 
+            // celularesToolStripMenuItem
+            // 
+            this.celularesToolStripMenuItem.Name = "celularesToolStripMenuItem";
+            this.celularesToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+            this.celularesToolStripMenuItem.Text = "Celulares";
+            // 
+            // descuentosToolStripMenuItem
+            // 
+            this.descuentosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4});
+            this.descuentosToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.descuentosToolStripMenuItem.Name = "descuentosToolStripMenuItem";
+            this.descuentosToolStripMenuItem.Size = new System.Drawing.Size(110, 25);
+            this.descuentosToolStripMenuItem.Text = "Descuentos";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(112, 26);
+            this.toolStripMenuItem2.Text = "10%";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(112, 26);
+            this.toolStripMenuItem3.Text = "20%";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(112, 26);
+            this.toolStripMenuItem4.Text = "30%";
+            // 
+            // empleadosToolStripMenuItem
+            // 
+            this.empleadosToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.empleadosToolStripMenuItem.Name = "empleadosToolStripMenuItem";
+            this.empleadosToolStripMenuItem.Size = new System.Drawing.Size(106, 25);
+            this.empleadosToolStripMenuItem.Text = "Empleados";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(71, 25);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(94, 25);
+            this.acercaDeToolStripMenuItem.Text = "AcercaDe";
+            // 
+            // sALIRToolStripMenuItem
+            // 
+            this.sALIRToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.sALIRToolStripMenuItem.Name = "sALIRToolStripMenuItem";
+            this.sALIRToolStripMenuItem.Size = new System.Drawing.Size(65, 25);
+            this.sALIRToolStripMenuItem.Text = "SALIR";
+            // 
+            // fmenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Venta_de_dispositivos_tecnologicos.Properties.Resources.fondoventa;
+            this.ClientSize = new System.Drawing.Size(790, 420);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "fmenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fmenu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem productosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pCsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem laptosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem celularesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem descuentosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem empleadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sALIRToolStripMenuItem;
+    }
+}
